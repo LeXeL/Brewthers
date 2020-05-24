487d277b8c87ff207a55372d8dc8c98cb93328e4
Diego Rodriguez <lexelEZ@gmail.com>
2020-05-24 17:04:40 -0500

    implemented structure

--- src/router.js
+++ src/router.js
@@ -1,30 +1,30 @@
-import Vue from "vue";
-import Router from "vue-router";
-import DefaultLayout from "./layouts/Default.vue";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from './layouts/HomePage.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      component: DefaultLayout,
-      children: [
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
         {
-          path: "",
-          name: "home",
-          component: Home,
+            path: '/',
+            component: HomePage,
+            children: [
+                {
+                    path: '',
+                    name: 'home',
+                    component: Home,
+                },
+                {
+                    path: '/about',
+                    name: 'about',
+                    component: About,
+                },
+            ],
         },
-        {
-          path: "/about",
-          name: "about",
-          component: About,
-        },
-      ],
-    },
-  ],
-});
+    ],
+})
