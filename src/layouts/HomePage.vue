00e59bbb3bb9fd1b709989c323d7c01eb1f1cf19
KidDarknezz <darkslave@live.com>
2020-05-25 19:25:20 -0500

    Landing sections created and hero on dev

--- src/layouts/HomePage.vue
+++ src/layouts/HomePage.vue
@@ -1,22 +1,64 @@
 <template>
-    <div>
-        <transition>
-            <img src="../assets/blog-bg-min.png" alt />
-        </transition>
-    </div>
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated>
+            <q-toolbar>
+                <q-toolbar-title>Quasar App</q-toolbar-title>
+                <div>Quasar v{{ $q.version }}</div>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <hero-section></hero-section>
+            <us-title-section></us-title-section>
+            <us-content-section></us-content-section>
+            <blog-title-section></blog-title-section>
+            <store-title-section></store-title-section>
+            <store-content-section></store-content-section>
+            <movingbeer-title-section></movingbeer-title-section>
+            <faq-section></faq-section>
+            <contact-section></contact-section>
+        </q-page-container>
+    </q-layout>
 </template>
 
-<style scoped>
-html,
+<script>
+import HeroSection from '@/views/landing/HeroSection.vue'
+import UsTitleSection from '@/views/landing/UsTitleSection.vue'
+import UsContentSection from '@/views/landing/UsContentSection.vue'
+import BlogTitleSection from '@/views/landing/BlogTitleSection.vue'
+import StoreTitleSection from '@/views/landing/StoreTitleSection.vue'
+import StoreContentSection from '@/views/landing/StoreContentSection.vue'
+import MovingbeerTitleSection from '@/views/landing/MovingbeerTitleSection.vue'
+import FaqSection from '@/views/landing/FaqSection.vue'
+import ContactSection from '@/views/landing/ContactSection.vue'
+
+export default {
+    components: {
+        'hero-section': HeroSection,
+        'us-title-section': UsTitleSection,
+        'us-content-section': UsContentSection,
+        'blog-title-section': BlogTitleSection,
+        'store-title-section': StoreTitleSection,
+        'store-content-section': StoreContentSection,
+        'movingbeer-title-section': MovingbeerTitleSection,
+        'faq-section': FaqSection,
+        'contact-section': ContactSection,
+    },
+}
+</script>
+
+<style>
+* {
+    color: #fff;
+}
+
 body {
-    margin: 0;
-    height: 100%;
+    background-color: #1f2120;
 }
-img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    position: absolute;
+
+.patternBg {
+    background-image: url('../assets/pattern.png');
+    height: 910px;
 }
+>>>>>>> Landing sections created and hero on dev
 </style>
---
+++ src/views/landing/UsTitleSection.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>This is the us title section</p>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
