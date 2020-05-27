cca5a36b7f2d2f8b9731e7eb8d392630e4c8fcd6
KidDarknezz <darkslave@live.com>
2020-05-27 18:43:27 -0500

    Contact section - still on dev

--- src/views/landing/ContactSection.vue
+++ src/views/landing/ContactSection.vue
@@ -1,9 +1,65 @@
 <template>
-    <p>This is the contact section</p>
+    <div class="pattern-bg contact-form">
+        <q-form>
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <q-input class="q-pa-md" color="white" dark filled label="Nombre" />
+                    </div>
+                    <div class="col">
+                        <q-input class="q-pa-md" color="white" dark filled label="Apellido" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <q-input
+                            class="q-pa-md"
+                            color="white"
+                            dark
+                            filled
+                            label="Correo electronico"
+                        />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <q-select
+                            class="q-pa-md"
+                            color="white"
+                            dark
+                            filled
+                            :options="options"
+                            label="Razon de contacto"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <q-input
+                            class="q-pa-md"
+                            color="white"
+                            dark
+                            filled
+                            type="textarea"
+                            label="Mensaje"
+                        />
+                    </div>
+                </div>
+            </div>
+        </q-form>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            options: ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'],
+        }
+    },
+}
 </script>
 
 <style></style>
