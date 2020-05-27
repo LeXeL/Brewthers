ab25e09b9f43cbc4e08c4ecc78d0801e6b27a924
KidDarknezz <darkslave@live.com>
2020-05-27 15:41:12 -0500

    Desktop navbar added

--- src/views/landing/HeroSection.vue
+++ src/views/landing/HeroSection.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="pattern-bg" style="position: relative">
-        <img class="absolute-center" src="@/assets/brewthers-logo.png" width="45%" />
+    <div>
+        <div class="pattern-bg desktop-only" style="position: relative;">
+            <img class="absolute-center" src="@/assets/brewthers-logo.png" width="45%" />
+        </div>
+        <div class="pattern-bg mobile-only" style="position: relative; height: 600px;">
+            <img class="absolute-center" src="@/assets/brewthers-logo.png" width="45%" />
+        </div>
     </div>
 </template>
