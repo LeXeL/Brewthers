34e134869fb1d1a230b48388b4c7430aeca3ec51
KidDarknezz <darkslave@live.com>
2020-05-27 15:41:12 -0500

    Desktop navbar added

--- src/views/landing/MovingbeerTitleSection.vue
+++ src/views/landing/MovingbeerTitleSection.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="movingbeer-bg" style="position: relative">
-        <h2 class="title-section absolute-center">movingbeer</h2>
+        <h2 class="title-section absolute-center desktop-only">movingbeer</h2>
+        <h3 class="title-section absolute-center mobile-only">movingbeer</h3>
         <a href="#" class="title-section-link absolute-bottom q-pb-xl">Explorar mas</a>
     </div>
 </template>
