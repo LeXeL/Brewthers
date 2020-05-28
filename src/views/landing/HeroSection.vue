93ad595856050b39d62d0f7d8a6b0b5f8e50bf7d
KidDarknezz <darkslave@live.com>
2020-05-28 13:38:58 -0500

    Visial fix on section backgrounds

--- src/views/landing/HeroSection.vue
+++ src/views/landing/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="pattern-bg desktop-only" style="position: relative;">
+        <div class="pattern-bg desktop-only" style="position: relative; background-size: cover;">
             <img class="absolute-center" src="@/assets/brewthers-logo.png" width="45%" />
         </div>
         <div class="pattern-bg mobile-only" style="position: relative; height: 600px;">
