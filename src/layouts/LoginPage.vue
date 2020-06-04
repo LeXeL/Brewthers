d9d4f53e61be9dcb59a686de93c49a09aa4060c6
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2020-06-03 21:08:06 -0500

    create account and login views

---
+++ src/layouts/LoginPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-layout>
+        <q-page-container>
+            <router-view></router-view>
+        </q-page-container>
+    </q-layout>
+</template>
+
+
+
+<style lang="scss" scoped>
+</style>
