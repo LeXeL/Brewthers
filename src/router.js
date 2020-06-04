031ac954d338a25f54721717bebadfa85a6ef70e
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2020-06-03 21:07:52 -0500

    created new routes for login and create account

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//Layouts
 import HomePage from './layouts/HomePage.vue'
+import LoginPage from './layouts/LoginPage.vue'
+//Views
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Login from './views/Login/Login.vue'
+import CreateAccount from './views/Login/CreateAccount.vue'
 
 Vue.use(Router)
 
@@ -10,6 +15,22 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
+        {
+            path: '/login',
+            component: LoginPage,
+            children: [
+                {
+                    path: '/login',
+                    name: 'login',
+                    component: Login,
+                },
+                {
+                    path: '/createaccount',
+                    name: 'create account',
+                    component: CreateAccount,
+                },
+            ],
+        },
         {
             path: '/',
             component: HomePage,
