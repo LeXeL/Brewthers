7d0de479284bbfc7bd77f5ce00a57da9ded6b28d
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2020-06-03 23:26:58 -0500

    added createUserOnDatabase

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,8 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+//Create User on the database
+exports.createUserOnDatabase = functions.auth.user().onCreate(user => {
+    console.log(`Called on user: ${JSON.stringify(user)}`)
+})
