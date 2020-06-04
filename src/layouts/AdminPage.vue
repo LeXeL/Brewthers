d375e57d039ac968cb7197ef498825e7e435154b
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2020-06-04 01:31:24 -0500

    implemented safe guards for routes

--- src/layouts/AdminPage.vue
+++ src/layouts/AdminPage.vue
@@ -1,9 +1,34 @@
 <template>
-    <div></div>
+    <q-layout>
+        <q-page-container>
+            SUPER SECRET ADMIN
+            <q-page>
+                <q-btn @click="logout()" color="primary">logout</q-btn>
+            </q-page>
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script>
-export default {}
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+    methods: {
+        async logout() {
+            firebase
+                .auth()
+                .signOut()
+                .then(async () => {
+                    await this.$store.dispatch('UserLogout')
+                    this.$router.push('/login')
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+    },
+}
 </script>
 
 <style lang="scss" scoped>
