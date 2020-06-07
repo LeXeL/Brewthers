24dc0a9b860e034b47317e94c3a9619621dca494
KidDarknezz <darkslave@live.com>
2020-06-07 13:15:41 -0500

    Make hero logo smaller

--- src/views/landing/HeroSection.vue
+++ src/views/landing/HeroSection.vue
@@ -3,39 +3,19 @@
         <div class="hero-stripes desktop-only" style="left: 50px;"></div>
         <div class="hero-stripes desktop-only" style="left: 90px;"></div>
         <div class="hero-stripes desktop-only" style="left: 130px;"></div>
-        <div
-            class="pattern-bg desktop-only"
-            style="background-size: cover; height: 930px;"
-        >
+        <div class="pattern-bg desktop-only" style="background-size: cover; height: 930px;">
             <div class="absolute-center row justify-center">
-                <img
-                    class="q-mb-xl"
-                    src="@/assets/brewthers-logo.png"
-                    width="60%"
-                />
-                <div
-                    class="text-h4"
-                    style="font-family: GilroyExtraBold; text-align: center;"
-                >
+                <img class="q-mb-xl" src="@/assets/brewthers-logo.png" width="40%" />
+                <div class="text-h4" style="font-family: GilroyExtraBold; text-align: center;">
                     Somos el punto de conexión entre ustedes y las buenas
                     cervezas artesanales panameñas.
                 </div>
             </div>
         </div>
-        <div
-            class="pattern-bg mobile-only"
-            style="background-size: cover; height: 600px;"
-        >
+        <div class="pattern-bg mobile-only" style="background-size: cover; height: 600px;">
             <div class="absolute-center row justify-center full-width">
-                <img
-                    class="q-mb-lg"
-                    src="@/assets/brewthers-logo.png"
-                    width="60%"
-                />
-                <div
-                    class="text-h6"
-                    style="font-family: GilroyExtraBold; text-align: center;"
-                >
+                <img class="q-mb-lg" src="@/assets/brewthers-logo.png" width="60%" />
+                <div class="text-h6" style="font-family: GilroyExtraBold; text-align: center;">
                     Somos el punto de conexión entre ustedes y las buenas
                     cervezas artesanales panameñas.
                 </div>
@@ -45,6 +25,6 @@
             <div class="absolute-center full-width">
                 <img src="@/assets/brewthers-logo.png" width="60%" />
             </div>
-        </div> -->
+        </div>-->
     </div>
 </template>
