6ed85c500fc625b7cbde094af92db2168818d406
KidDarknezz <darkslave@live.com>
2020-06-08 23:10:03 -0500

    Remove global css tags and new logo

--- src/layouts/HomePage.vue
+++ src/layouts/HomePage.vue
@@ -1,13 +1,8 @@
 <template>
-    <q-layout view="lHh Lpr lFf">
+    <q-layout view="lHh Lpr lFf" class="brewthers-dark-bg">
         <q-header elevated>
-            <q-toolbar
-                class="text-white shadow-2 desktop-only"
-                style="background-color: #111"
-            >
-                <q-avatar>
-                    <img src="../assets/brewthers-logo.png" />
-                </q-avatar>
+            <q-toolbar class="text-white shadow-2 desktop-only" style="background-color: #111">
+                <img src="../assets/logo-horizontal.png" width="10%" />
                 <q-space />
                 <q-tabs class="brewthers-nav">
                     <a
@@ -20,20 +15,11 @@
                             <span>{{ navlink.text }}</span>
                         </q-tab>
                     </a>
-                    <q-btn
-                        color="primary"
-                        text-color="black"
-                        label="iniciar sesión"
-                    />
+                    <q-btn color="primary" text-color="black" label="iniciar sesión" />
                 </q-tabs>
             </q-toolbar>
-            <q-toolbar
-                class="text-white shadow-2 mobile-only"
-                style="background-color: #111"
-            >
-                <q-avatar>
-                    <img src="../assets/brewthers-logo.png" />
-                </q-avatar>
+            <q-toolbar class="text-white shadow-2 mobile-only" style="background-color: #111">
+                <img src="../assets/logo-horizontal.png" width="50%" />
                 <q-space />
                 <q-btn flat round dense @click="dialog = true">
                     <i class="fas fa-bars" style="color: #27a3c3 "></i>
@@ -57,24 +43,20 @@
                             </a>
                         </li>
                         <li v-for="(navlink, i) in navLinks" :key="i">
-                            <a :href="navlink.ref" @click="dialog = false">{{
+                            <a :href="navlink.ref" @click="dialog = false">
+                                {{
                                 navlink.text
-                            }}</a>
+                                }}
+                            </a>
                         </li>
                         <li>
-                            <a href="http://google.com" @click="dialog = false"
-                                >iniciar sesion</a
-                            >
+                            <a href="http://google.com" @click="dialog = false">iniciar sesion</a>
                         </li>
                         <li class="q-mt-md">
                             <a href="#" @click="dialog = false">
                                 <i class="fab fa-facebook"></i>
                             </a>
-                            <a
-                                href="#"
-                                class="on-right"
-                                @click="dialog = false"
-                            >
+                            <a href="#" class="on-right" @click="dialog = false">
                                 <i class="fab fa-instagram"></i>
                             </a>
                         </li>
@@ -85,24 +67,16 @@
 
         <q-page-container>
             <hero-section id="hero"></hero-section>
-            <us-title-section
-                id="title"
-                @openSection="toggleUsSection()"
-            ></us-title-section>
+            <us-title-section id="title" @openSection="toggleUsSection()"></us-title-section>
             <q-slide-transition>
                 <div v-show="usSection">
-                    <us-content-section
-                        id="uscontent"
-                        @openSection="toggleUsSection()"
-                    ></us-content-section>
+                    <us-content-section id="uscontent" @openSection="toggleUsSection()"></us-content-section>
                 </div>
             </q-slide-transition>
             <blog-title-section id="blog"></blog-title-section>
             <!-- <store-title-section></store-title-section> -->
             <!-- <store-content-section></store-content-section> -->
-            <movingbeer-title-section
-                id="movingbeer"
-            ></movingbeer-title-section>
+            <movingbeer-title-section id="movingbeer"></movingbeer-title-section>
             <faq-section id="faq"></faq-section>
             <contact-section id="contact"></contact-section>
             <footer>
@@ -111,17 +85,14 @@
                         <img src="../assets/brewthers-logo.png" width="60%" />
                     </div>
                     <div class="col-lg-3 col-md-4 q-pr-md">
-                        <div
-                            class="text-h6"
-                            style="font-family: GilroyExtraBold"
-                        >
-                            Navegación
-                        </div>
+                        <div class="text-h6" style="font-family: GilroyExtraBold">Navegación</div>
                         <ul>
                             <li v-for="(link, i) in navLinks" :key="i">
-                                <a :href="link.ref" v-smooth-scroll>{{
+                                <a :href="link.ref" v-smooth-scroll>
+                                    {{
                                     link.text
-                                }}</a>
+                                    }}
+                                </a>
                             </li>
                             <li>
                                 <a href="#">terminos y condiciones</a>
@@ -129,12 +100,7 @@
                         </ul>
                     </div>
                     <div class="col-lg-3 col-md-4 q-pr-md">
-                        <div
-                            class="text-h6"
-                            style="font-family: GilroyExtraBold"
-                        >
-                            Contáctanos
-                        </div>
+                        <div class="text-h6" style="font-family: GilroyExtraBold">Contáctanos</div>
                         <p style="margin-top: 12px;">
                             Panama, San Francisco, Calle 23 Sur, Torre Something
                             Ofc. 3D
@@ -146,28 +112,21 @@
                         <div
                             class="text-h6"
                             style="font-family: GilroyExtraBold; margin-bottom: 12px;"
-                        >
-                            Conectate
-                        </div>
+                        >Conectate</div>
                         <a href="#">
-                            <i
-                                class="fab fa-instagram"
-                                style="font-size: 40px;"
-                            ></i>
+                            <i class="fab fa-instagram" style="font-size: 40px;"></i>
                         </a>
                         <a href="#" class="on-right">
-                            <i
-                                class="fab fa-facebook"
-                                style="font-size: 40px;"
-                            ></i>
+                            <i class="fab fa-facebook" style="font-size: 40px;"></i>
                         </a>
                         <br />
                         <br />
                         <span>
                             {{ new Date().getFullYear() }} &copy; Developed by.
-                            <a href="#" style="color: #27a3c3"
-                                >BlueBaloon Inc.</a
-                            >
+                            <a
+                                href="#"
+                                style="color: #27a3c3"
+                            >BlueBaloon Inc.</a>
                         </span>
                     </div>
                 </div>
