6cd1b54d177d3dc5f00ecbfa4bb884da45d495b3
KidDarknezz <darkslave@live.com>
2020-06-10 22:51:08 -0500

    Tiendita title section

--- src/views/landing/StoreTitleSection.vue
+++ src/views/landing/StoreTitleSection.vue
@@ -1,5 +1,24 @@
 <template>
-    <p>This is the store title section</p>
+    <div class="tiendita-bg flex" style="position: relative">
+        <div class="absolute-center">
+            <div class="text-h2 title-section desktop-only q-mb-md">
+                tiendita
+            </div>
+            <div class="text-h3 title-section mobile-only q-mb-md">
+                tiendita
+            </div>
+            <div
+                class="text-h4 q-mb-lg"
+                style="font-family: GilroyExtraBold; text-align: center;"
+            >
+                ¡Llegaste donde tenemos pa’ ti y pa’ regala’!
+            </div>
+        </div>
+
+        <a href="#" class="title-section-link absolute-bottom q-pb-xl"
+            >Explorar mas</a
+        >
+    </div>
 </template>
 
 <script>
