b6b912876f2a4db489ce6c758c0b0a93b0765044
KidDarknezz <darkslave@live.com>
2020-06-12 14:01:02 -0500

    Store index fix images padding

--- src/views/movingbeer/StoreIndex.vue
+++ src/views/movingbeer/StoreIndex.vue
@@ -1,12 +1,17 @@
 <template>
     <q-page class="movingbeer-pattern-bg">
         <q-carousel arrows animated v-model="slide" height="600px">
-            <q-carousel-slide name="first" :img-src="require('@/assets/movingbeer-bg.jpg')">
+            <q-carousel-slide
+                name="first"
+                :img-src="require('@/assets/movingbeer-bg.jpg')"
+            >
                 <div class="absolute-center full-width custom-caption">
                     <div
                         class="text-h2"
                         style="font-family: GilroyExtraBold; text-transform: uppercase"
-                    >Bienvenido a movingbeer</div>
+                    >
+                        Bienvenido a movingbeer
+                    </div>
                 </div>
             </q-carousel-slide>
         </q-carousel>
@@ -17,24 +22,31 @@
                     <div
                         class="text-h3 text-center"
                         style="font-family: GilroyExtraBold; text-transform: uppercase;"
-                    >Familia brewthers</div>
+                    >
+                        Familia brewthers
+                    </div>
                 </div>
             </div>
-            <div class="row q-pa-xl">
-                <div class="col-lg-3" v-for="(brew, i) in brewers" :key="i">
+            <div class="row">
+                <div
+                    class="col-lg-3 col-sm-3 col-xs-12"
+                    v-for="(brew, i) in brewers"
+                    :key="i"
+                >
                     <q-card
-                        class="my-card q-mb-xl justify-center movingbeer-tile"
+                        class="movingbeer-tile full-width q-pa-xl"
                         @click="dialog = true"
                     >
                         <q-img
-                            :src="require(`@/assets/familia-brewthers/${brew.logo}`)"
-                            width="100%"
-                            class
-                            basic
+                            :src="
+                                require(`@/assets/familia-brewthers/${brew.logo}`)
+                            "
                         >
                             <div
                                 class="absolute-bottom text-h6 text-center movingbeer-tile-overlay"
-                            >{{ brew.name }}</div>
+                            >
+                                {{ brew.name }}
+                            </div>
                         </q-img>
                     </q-card>
                 </div>
@@ -52,7 +64,9 @@
                     <q-space />
                     <q-btn dense flat v-close-popup>
                         <i class="fas fa-times" style="font-size: 25px;"></i>
-                        <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
+                        <q-tooltip content-class="bg-white text-primary"
+                            >Close</q-tooltip
+                        >
                     </q-btn>
                 </q-bar>
 
