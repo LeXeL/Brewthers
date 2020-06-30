7a945255564ea4a40c77a0e98af99d8392983972
KidDarknezz <darkslave@live.com>
2020-06-30 14:11:21 -0500

    Account rejection reazons

--- src/views/admin/AccountDetaills.vue
+++ src/views/admin/AccountDetaills.vue
@@ -3,40 +3,81 @@
         <div class="text-h5 q-mb-md">Detalles de cuenta</div>
         <div class="row">
             <div class="col-lg-4 q-pa-md">
-                <q-input
-                    filled
-                    label="Nombre del restaurante"
-                    value="El Fogon de Tia Chencha"
-                    class="q-mb-md"
-                    dark
-                    disable
-                />
-                <q-input filled label="RUC" value="65-46518656-2019" class="q-mb-md" dark disable />
-                <q-input
-                    filled
-                    label="Persona de contacto"
-                    value="Chencha Perez"
-                    class="q-mb-md"
-                    dark
-                    disable
-                />
-                <q-input
-                    filled
-                    label="Correo"
-                    value="tia.chencha@gmail.com"
-                    class="q-mb-md"
-                    dark
-                    disable
-                />
-                <q-input filled label="Celular" value="6565-6565" class="q-mb-md" dark disable />
-                <q-input
-                    filled
-                    label="Direccion"
-                    value="Arribita de la Delta"
-                    class="q-mb-md"
-                    dark
-                    disable
-                />
+                <div class="row q-mb-md">
+                    <q-input
+                        filled
+                        label="Nombre del restaurante"
+                        value="El Fogon de Tia Chencha"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="RUC"
+                        value="65-46518656-2019"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="Persona de contacto"
+                        value="Chencha Perez"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="Correo"
+                        value="tia.chencha@gmail.com"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="Celular"
+                        value="6565-6565"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="Direccion"
+                        value="Arribita de la Delta"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                    <q-input
+                        filled
+                        label="Estatus"
+                        value="Activa"
+                        class="q-mb-md full-width"
+                        dark
+                        readonly
+                    />
+                </div>
+
+                <div class="row">
+                    <div class="text-h6">Motivos de rechazo o deshabilitacion de cuenta</div>
+                    <q-list dark padding class="full-width">
+                        <q-item clickable v-ripple>
+                            <q-item-section>Motivo de rechazo numero 1</q-item-section>
+                        </q-item>
+
+                        <q-item clickable v-ripple>
+                            <q-item-section>Motivo de rechazo numero 2</q-item-section>
+                        </q-item>
+
+                        <q-item clickable v-ripple>
+                            <q-item-section>Motivo de rechazo numero 3</q-item-section>
+                        </q-item>
+                    </q-list>
+                </div>
             </div>
             <div class="col-lg-8 q-pa-md">
                 <div class="row q-mb-md">
@@ -72,7 +113,13 @@
                 </q-card-section>
 
                 <q-card-section class="q-pt-none">
-                    <q-input filled v-model="address" autofocus @keyup.enter="prompt = false" dark />
+                    <q-option-group
+                        :options="options"
+                        label="Notifications"
+                        type="checkbox"
+                        dark
+                        v-model="group"
+                    />
                 </q-card-section>
 
                 <q-card-actions align="right" class="text-primary">
@@ -89,6 +136,12 @@ export default {
     data() {
         return {
             rejectDialog: false,
+            group: [],
+            options: [
+                {label: 'Motivo de rechazo 1', value: 'op1'},
+                {label: 'Motivo de rechazo 2', value: 'op2'},
+                {label: 'Motivo de rechazo 3', value: 'op3'},
+            ],
         }
     },
 }
