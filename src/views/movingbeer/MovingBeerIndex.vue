aa20ca39e299e30c334eb5e20ea01000b65e41fa
Diego Rodriguez <lexelEZ@gmail.com>
2020-06-30 22:50:22 -0500

    Remove from index component

--- src/views/movingbeer/MovingBeerIndex.vue
+++ src/views/movingbeer/MovingBeerIndex.vue
@@ -1,9 +1,6 @@
 <template>
     <q-page class="movingbeer-pattern-bg">
-        <loading-alert :display="loadingAlert" />
         <carousel />
-        <brewthers-alert :title="alert.title" :message="alert.message" :display="alert.display" />
-        <q-btn color="primary" label="Trigger alert" @click="triggerAlert" />
         <section>
             <div class="row q-pt-xl">
                 <div class="col">
