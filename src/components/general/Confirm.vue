8402573138edca482f5d3f7d080af207f6ef598a
KidDarknezz <darkslave@live.com>
2020-07-01 13:01:55 -0500

    Display confirm trigger handler

--- src/components/general/Confirm.vue
+++ src/components/general/Confirm.vue
@@ -1,29 +1,32 @@
 <template>
-<q-dialog v-model="confirm">
-      <q-card dark>
-        <q-card-section>
-          <div class="text-h6">{{ title }}</div>
-        </q-card-section>
+    <q-dialog v-model="confirm">
+        <q-card dark>
+            <q-card-section>
+                <div class="text-h6">{{ title }}</div>
+            </q-card-section>
 
-        <q-card-section class="q-pt-none">
-          {{ message }}
-        </q-card-section>
+            <q-card-section class="q-pt-none">{{ message }}</q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn label="Aceptar" color="secondary" v-close-popup />
-          <q-btn label="Cancelar" color="red-7" v-close-popup />
-        </q-card-actions>
-      </q-card>
+            <q-card-actions align="right">
+                <q-btn label="Aceptar" color="secondary" v-close-popup />
+                <q-btn label="Cancelar" color="red-7" v-close-popup />
+            </q-card-actions>
+        </q-card>
     </q-dialog>
 </template>
 
 <script>
-    export default {
-        props: ['title', 'message', 'display'],
-        data() {
-            return {
-                confirm: true
-            }
+export default {
+    props: ['title', 'message', 'display'],
+    data() {
+        return {
+            confirm: false,
         }
-    }
+    },
+    watch: {
+        display: function() {
+            this.confirm = !this.confirm
+        },
+    },
+}
 </script>
