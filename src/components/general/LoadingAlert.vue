90716caa76af71d3f20e61b91ea932a52058ec9c
Diego Rodriguez <lexelEZ@gmail.com>
2020-06-30 22:49:22 -0500

    clean up code

--- src/components/general/LoadingAlert.vue
+++ src/components/general/LoadingAlert.vue
@@ -1,21 +1,16 @@
 <template>
     <q-dialog v-model="display" persistent>
-      <q-card dark style="width: auto;">
-        <q-card-section>
-          <div class="text-h6 q-mr-lg q-mt-sm" style="display: inline-block">CARGANDO</div>
-          <q-spinner color="primary" size="3em" :thickness="10" />
-        </q-card-section>
-      </q-card>
+        <q-card dark style="width: auto;">
+            <q-card-section>
+                <div class="text-h6 q-mr-lg q-mt-sm" style="display: inline-block">CARGANDO</div>
+                <q-spinner color="primary" size="3em" :thickness="10" />
+            </q-card-section>
+        </q-card>
     </q-dialog>
 </template>
 
 <script>
-    export default {
-      props: ['display'],
-        data() {
-            return {
-                alert: true,
-            }
-        }
-    }
+export default {
+    props: ['display'],
+}
 </script>
