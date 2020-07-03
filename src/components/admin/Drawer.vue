d1d8425738db945bb5921ab1c86d781f83c2bc16
KidDarknezz <darkslave@live.com>
2020-07-03 14:49:46 -0500

    Route and layout

--- src/components/admin/Drawer.vue
+++ src/components/admin/Drawer.vue
@@ -20,7 +20,7 @@
                     </q-avatar>
                     <q-item-section>Pedidos</q-item-section>
                 </q-item>
-                <q-item active clickable v-ripple>
+                <q-item active clickable v-ripple to="/inventory-manager">
                     <q-avatar rounded>
                         <i class="fas fa-boxes" style="font-size: 15px;"></i>
                     </q-avatar>
---
+++ src/views/admin/InventoryManager.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="q-pa-md">
+        <div class="text-h5 q-mb-md">Administrador de inventario</div>
+        <div class="row">
+            <div class="col-lg-8 q-pa-md">
+                <!-- <brewing-houses-table /> -->
+                <p>table</p>
+            </div>
+            <div class="col-lg-4 q-pa-md">
+                <!-- <brewing-houses-form /> -->
+                <p>form</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import BrewingHousesTable from '@/components/admin/BrewingHousesTable'
+// import BrewingHousesForm from '@/components/admin/BrewingHousesForm'
+
+export default {
+    // components: {
+    //     'brewing-houses-table': BrewingHousesTable,
+    //     'brewing-houses-form': BrewingHousesForm,
+    // },
+}
+</script>
