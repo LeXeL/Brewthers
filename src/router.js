fbb89a4bbef3bd1b932068c54b8a2f7d7f9bc48e
KidDarknezz <darkslave@live.com>
2020-07-03 12:04:13 -0500

    Brewing houses route

--- src/router.js
+++ src/router.js
@@ -20,6 +20,7 @@ import MovingBeerHistory from './views/movingbeer/MovingBeerHistory.vue'
 import AdminIndex from './views/admin/Index'
 import AccountsManager from './views/admin/AccountsManager'
 import AccountDetails from './views/admin/AccountDetails'
+import BrewingHouses from './views/admin/BrewingHouses'
 
 Vue.use(Router)
 
@@ -57,6 +58,11 @@ export default new Router({
                     path: '/account-details',
                     name: 'account-details',
                     component: AccountDetails,
+                },
+                {
+                    path: '/brewing-houses',
+                    name: 'brewing-houses',
+                    component: BrewingHouses,
                 },
             ],
         },
