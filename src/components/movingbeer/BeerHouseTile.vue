e110525f50f0917d88276d956ba39acd13a0ce22
Diego Rodriguez <lexelEZ@gmail.com>
2020-07-17 00:25:07 -0500

    Implemented catalog for products

--- src/components/movingbeer/BeerHouseTile.vue
+++ src/components/movingbeer/BeerHouseTile.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-card class="movingbeer-tile full-width q-pa-xl" @click="$emit('openBeerHouse', brew.name)">
+    <q-card class="movingbeer-tile full-width q-pa-xl" @click="$emit('openBeerHouse', brew.id)">
         <q-img :src="brew.photoLocation">
             <div class="absolute-bottom text-h6 text-center movingbeer-tile-overlay">{{ brew.name }}</div>
         </q-img>
@@ -8,6 +8,14 @@
 
 <script>
 export default {
-    props: ['brew'],
+    props: {
+        brew: {
+            type: Object,
+            default: () => {
+                return {}
+            },
+        },
+    },
+    // props: ['brew'],
 }
 </script>
