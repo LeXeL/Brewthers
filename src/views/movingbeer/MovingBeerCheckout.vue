ceb3ab5e966d6537d02e458219356b79c19fa042
KidDarknezz <darkslave@live.com>
2020-07-19 23:52:14 -0500

    Checkout view responsive fix

--- src/views/movingbeer/MovingBeerCheckout.vue
+++ src/views/movingbeer/MovingBeerCheckout.vue
@@ -1,71 +1,76 @@
 <template>
-    <q-page class="q-pa-xl pattern-bg">
-        <div class="row">
-            <div class="col"></div>
-            <div class="col-lg-5">
-                <div class="text-h3 q-mb-xl">{{ title }}</div>
+    <q-page class="q-pa-md pattern-bg" style="height: auto; background-repeat: repeat-y">
+        <div class="row q-pt-xl">
+            <div class="col desktop-only"></div>
+            <div class="col-lg-8 col-xs-12">
+                <div class="text-h4 q-mb-xl">{{ title }}</div>
             </div>
-            <div class="col-lg-2"></div>
-            <div class="col"></div>
+            <div class="col desktop-only"></div>
         </div>
-        <div class="row">
-            <div class="col"></div>
-            <div class="col-lg-5">
-                <div class="text-h5 q-pl-md">Detalle de articulos:</div>
-                <order-item-details v-for="(item, i) in 5" :key="i" />
-            </div>
-            <div class="col-lg-2">
-                <div class="q-mb-xl">
-                    <div class="text-h5 q-mb-md">Detalle de orden:</div>
-                    <div class="text-subitle-2">Articulos (10): $ 150.00</div>
-                    <div class="text-subitle-2">ITBMS: $ 10.50</div>
-                    <div class="text-h6">Total: $ 160.50</div>
-                </div>
 
-                <div class="row" v-if="orderStatus === 'open'">
-                    <div class="text-h5 q-mb-md">Metodo de pago:</div>
+        <div class="row">
+            <div class="col desktop-only"></div>
+
+            <div class="col-lg-8 col-sm-12 col-xs-12">
+                <div class="row">
+                    <div class="col-lg-8 col-sm-8 col-xs-12 q-mb-md">
+                        <div class="text-h5 q-pl-md">Detalle de articulos:</div>
+                        <order-item-details v-for="(item, i) in 5" :key="i" />
+                    </div>
+                    <div class="col-lg-4 col-sm-4 col-xs-12">
+                        <div class="q-mb-xl">
+                            <div class="text-h5 q-mb-md">Detalle de orden:</div>
+                            <div class="text-subitle-2">Articulos (10): $ 150.00</div>
+                            <div class="text-subitle-2">ITBMS: $ 10.50</div>
+                            <div class="text-h6">Total: $ 160.50</div>
+                        </div>
+                        <div class="row" v-if="orderStatus === 'open'">
+                            <div class="text-h5 q-mb-md">Metodo de pago:</div>
+                        </div>
+                        <div class="row q-mb-lg" v-if="orderStatus === 'open'">
+                            <q-option-group
+                                :options="paymentOptions"
+                                label="Notifications"
+                                type="radio"
+                                v-model="paymentMethod"
+                            />
+                        </div>
+                        <div
+                            class="row"
+                            v-if="proofRequired.includes(paymentMethod) && orderStatus === 'open'"
+                        >
+                            <div class="text-h5 q-mb-md">Comprobante de pago:</div>
+                        </div>
+                        <div
+                            class="row q-mb-lg"
+                            v-if="proofRequired.includes(paymentMethod) && orderStatus === 'open'"
+                        >
+                            <q-file outlined color="white" dark label="Adjuntar comprobante">
+                                <template v-slot:prepend>
+                                    <i class="fas fa-paperclip"></i>
+                                </template>
+                            </q-file>
+                        </div>
+                        <q-btn
+                            class="full-width q-mb-lg"
+                            color="primary"
+                            label="Enviar orden"
+                            @click="sendOrder"
+                            v-if="orderStatus == 'open'"
+                        />
+                        <q-btn
+                            class="full-width q-mb-lg"
+                            color="primary"
+                            label="Volver a Movingbeer"
+                            v-else
+                            to="/movingbeer"
+                        />
+                    </div>
                 </div>
-                <div class="row q-mb-lg" v-if="orderStatus === 'open'">
-                    <q-option-group
-                        :options="paymentOptions"
-                        label="Notifications"
-                        type="radio"
-                        v-model="paymentMethod"
-                    />
-                </div>
-                <div
-                    class="row"
-                    v-if="proofRequired.includes(paymentMethod) && orderStatus === 'open'"
-                >
-                    <div class="text-h5 q-mb-md">Comprobante de pago:</div>
-                </div>
-                <div
-                    class="row q-mb-lg"
-                    v-if="proofRequired.includes(paymentMethod) && orderStatus === 'open'"
-                >
-                    <q-file outlined color="white" dark label="Adjuntar comprobante">
-                        <template v-slot:prepend>
-                            <i class="fas fa-paperclip"></i>
-                        </template>
-                    </q-file>
-                </div>
-                <q-btn
-                    class="full-width"
-                    color="primary"
-                    label="Enviar orden"
-                    @click="sendOrder"
-                    v-if="orderStatus == 'open'"
-                />
-                <q-btn
-                    class="full-width"
-                    color="primary"
-                    label="Volver a Movingbeer"
-                    v-else
-                    to="/movingbeer"
-                />
             </div>
-            <div class="col"></div>
+            <div class="col desktop-only"></div>
         </div>
+
         <q-dialog v-model="orderConfirmationDialog">
             <q-card dark class="text-white">
                 <q-card-section>
