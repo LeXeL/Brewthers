39130542da07e34df47cf73655179a65e9e9151a
Diego Rodriguez <lexelEZ@gmail.com>
2020-08-19 14:12:08 -0500

    Added tofixed to prices in order details

--- src/components/general/OrderItemDetails.vue
+++ src/components/general/OrderItemDetails.vue
@@ -9,7 +9,10 @@
             <div class="text-subtitle-2" v-if="data.style">Estilo: {{data.style}}</div>
             <div class="text-subtitle-2" v-if="data.abv">ABV: {{data.abv}}%</div>
             <div class="text-subtitle-2" v-if="data.ibu">IBU: {{data.ibu}}</div>
-            <div class="text-subtitle-2" v-if="data.price">Precio unitario: $ {{data.price}}</div>
+            <div
+                class="text-subtitle-2"
+                v-if="data.price"
+            >Precio unitario: $ {{parseFloat(data.price).toFixed(2) }}</div>
             <div class="text-subtitle-2" v-if="data.amount">Cantidad: {{data.amount}}</div>
             <div class="text-subtitle-2" v-if="$route.fullPath.includes('order-details') ">
                 <q-btn color="red-7" size="xs" @click="$emit('remove',data)" :disable="disableprop">
