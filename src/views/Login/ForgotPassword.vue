691438115a5db1335ea24633029b8e13bdbf1e2a
KidDarknezz <darkslave@live.com>
2020-09-16 11:19:47 -0500

    Fixed form

--- src/views/Login/ForgotPassword.vue
+++ src/views/Login/ForgotPassword.vue
@@ -13,8 +13,8 @@
             <div class="row">
                 <q-img :src="require('@/assets/logo-horizontal.png')" class="q-mb-lg" />
                 <q-card dark square class="q-pa-lg shadow-1">
-                    <q-card-section>
-                        <q-form class="q-gutter-md">
+                    <q-form @submit="resetPassword">
+                        <q-card-section>
                             <q-input
                                 dark
                                 square
@@ -26,18 +26,18 @@
                                         val => val.length > 0 || 'El campo es obligatorio',
                                     ]"
                             ></q-input>
-                        </q-form>
-                    </q-card-section>
-                    <q-card-actions class="q-px-md">
-                        <q-btn
-                            unelevated
-                            color="primary"
-                            size="lg"
-                            class="full-width"
-                            label="Enviar Correo"
-                            @click="resetPassword"
-                        />
-                    </q-card-actions>
+                        </q-card-section>
+                        <q-card-actions class="q-px-md">
+                            <q-btn
+                                unelevated
+                                color="primary"
+                                size="lg"
+                                class="full-width"
+                                label="Enviar Correo"
+                                type="submit"
+                            />
+                        </q-card-actions>
+                    </q-form>
                 </q-card>
             </div>
         </div>
@@ -73,10 +73,11 @@ export default {
                     this.displayAlert = true
                 })
                 .catch(error => {
+                    this.displayLoading = false
                     console.log(error)
                     this.alertTitle = 'Hey AWANTA!'
                     this.alertMessage =
-                        'Hubo un error con tu peticion por favor intentalo mas tarde'
+                        'Hubo un error con tu peticion por favor intentalo mas tarde.'
                     this.alertType = 'error'
                     this.displayAlert = true
                 })
