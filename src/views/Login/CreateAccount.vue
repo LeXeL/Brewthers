9ef680a8cec00115b89f9e63202bc1740b049f79
KidDarknezz <darkslave@live.com>
2020-11-29 13:03:46 -0500

    fix typo

--- src/views/Login/CreateAccount.vue
+++ src/views/Login/CreateAccount.vue
@@ -207,7 +207,7 @@
                 </q-card-section>
 
                 <q-card-section class="q-pt-none">
-                    Hemos recivido tu información satisfactoriamente. Uno de
+                    Hemos recibido tu información satisfactoriamente. Uno de
                     nuestros administradores aprobará tu cuenta para que
                     empieces a realizar pedidos.
                     <br />
