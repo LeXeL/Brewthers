beb45816c5b879a5c67374511b88d6fd132389bb
Alejandro Millan <darkslave@live.com>
2020-12-02 10:20:58 -0500

    table created

--- src/views/admin/ExclusivenessAndPrices.vue
+++ src/views/admin/ExclusivenessAndPrices.vue
@@ -1,5 +1,93 @@
 <template>
     <q-page class="q-pa-md">
-        <p>exclusiveness</p>
+        <div class="text-h5 q-mb-md">Exclusividad y precios</div>
+        <div class="row q-mb-md">
+            <div class="col-lg-2 q-pa-md">
+                <q-input filled label="Nombre" color="primary" dark dense />
+            </div>
+            <div class="col-lg-2 q-pa-md">
+                <q-input filled label="Correo" color="primary" dark dense />
+            </div>
+        </div>
+        <div class="row q-px-md">
+            <div class="col">
+                <q-table
+                    title="Restaurantes"
+                    :data="data"
+                    :columns="columns"
+                    row-key="name"
+                    dark
+                    binary-state-sort
+                >
+                    <template v-slot:body="props">
+                        <q-tr :props="props">
+                            <q-td key="name" :props="props">
+                                {{ props.row.name }}
+                            </q-td>
+                            <q-td key="email" :props="props">
+                                {{ props.row.email }}
+                            </q-td>
+                            <q-td>
+                                <q-btn
+                                    color="primary"
+                                    label="Administrar"
+                                    size="xs"
+                                />
+                            </q-td>
+                        </q-tr>
+                    </template>
+                </q-table>
+            </div>
+        </div>
     </q-page>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            columns: [
+                {
+                    name: 'name',
+                    label: 'Nombre',
+                    field: 'name',
+                    align: 'left',
+                    sortable: true,
+                },
+                {
+                    name: 'email',
+                    label: 'Correo',
+                    align: 'left',
+                    field: 'email',
+                },
+                {
+                    label: 'Administrar',
+                    align: 'left',
+                },
+            ],
+            data: [
+                {
+                    name: 'Frozen Yogurt',
+                    email: 'asdf@asdf.com',
+                },
+                {
+                    name: 'Frozen Yogurt',
+                    email: 'asdf@asdf.com',
+                },
+                {
+                    name: 'Frozen Yogurt',
+                    email: 'asdf@asdf.com',
+                },
+                {
+                    name: 'Frozen Yogurt',
+                    email: 'asdf@asdf.com',
+                },
+                {
+                    name: 'Frozen Yogurt',
+                    email: 'asdf@asdf.com',
+                },
+            ],
+        }
+    },
+}
+</script>
