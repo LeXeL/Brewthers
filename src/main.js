2fc0779d6e1509979bb27f23440bf1602c46366c
Diego Rodriguez <--unset>
2021-01-31 14:01:30 -0500

    testing

--- src/main.js
+++ src/main.js
@@ -42,10 +42,13 @@ let configOptions = {
     measurementId: 'G-FSTF3WL646',
 }
 firebase.initializeApp(configOptions)
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV == 'development' || 'testing') {
-    // firebase.auth().useEmulator('http://localhost:9099/')
-    // firebase.firestore().useEmulator('localhost', 8081)
+
+if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'testing'
+) {
+    firebase.auth().useEmulator('http://localhost:9099/')
+    firebase.firestore().useEmulator('localhost', 8081)
 }
 
 firebase.analytics()
